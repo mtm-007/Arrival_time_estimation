843961185d8f077a9e44c0c52f1d3aafa5a28167
Merhawi <45622655+mtm-007@users.noreply.github.com>
2024-10-03 08:19:59 +0000

    inference flask webservice

--- src/predict.py
+++ src/predict.py
@@ -1,26 +1,30 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 import pickle
-import pyarrow
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 #parameters
 model_choice = 'linear_reg'
 out_put_file = f'model_type_{model_choice}.bin'
 
-#load model with pickle
 with open(f'../models/{out_put_file}', 'rb') as f_in:
     dv, Linear_R = pickle.load(f_in)
 
-inference_input =  'To be Defined'
-X_inf = dv.tranform([inference_input])
-Y_inf_pred = model.predict(X_inf)
+app = Flask('arrival_prediction')
+@app.route('/predict', methods=['POST'])
 
-print(f'Inference input: {inference_input}')
-print(f'Prediction output: {Y_inf_pred}s')
+def predict():
+    inference_input = request.get_json()
+
+    X_inf = dv.transform([inference_input])
+    y_pred = Linear_R.predict(X_inf)[0]
+    result  = {
+        "Prediction result": y_pred,
+    }
+    return jsonify(result)
+
+if __name__=='__main__':
+    app.run(debug=True, host = '0.0.0.0', port=9696)
+
