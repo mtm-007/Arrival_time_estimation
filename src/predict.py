6d5d044212aea78f00ee475d2c8c4b051f12faa0
Merhawi <merhawitsegay007@gmail.com>
2024-10-05 00:37:03 +0000

    add

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,8 @@
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 import pickle
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask,request, jsonify
+
 
 #parameters
 model_choice = 'linear_reg_w_PU_DO'
@@ -13,8 +12,6 @@ out_put_file = f'model_{model_choice}.bin'
 with open(f'{out_put_file}', 'rb') as f_in:
     dv, Linear_R = pickle.load(f_in)
 
-app = Flask('arrival_prediction')
-@app.route('/predict', methods=['POST'])
 
 def prepare_features(ride):
     features = {}
@@ -22,16 +19,28 @@ def prepare_features(ride):
     features['trip_distance'] = ride['trip_distance']
     return features
 
-def predict(inference_input):
-    #inference_input = request.get_json()
-
-    X_inf = dv.transform([inference_input])
+def predict(ride_input):
+    
+    X_inf = dv.transform([ride_input])
     y_pred = Linear_R.predict(X_inf)
-    result  = {
-        "Prediction result": float(y_pred),
+    
+    return float(y_pred[0])
+    
+
+app = Flask('arrival_prediction')
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+
+    result = {
+        "duration": pred,
     }
-    return result[0]
-    #return jsonify(result)
+
+    return jsonify(result)
 
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0', port=9696)
--- src/predict_test_01.py
+++ src/predict_test_01.py
@@ -1,20 +1,15 @@
 import requests
-import predict
-
-
-url = 'http://0.0.0.0:9696/predict'
 
 inference_input = {
-    "PULocationID": 110,
-    "DOLocationID": 50,
+    "PULocationID": 210,
+    "DOLocationID": 69,
     #"PUDO": '110_50',
-    "trip_distance": 59.98
+    "trip_distance": 9.98
 }
 
-features = predict.prepare_features(inference_input)
-pred = predict.predict(features)
-print(f'From new_test: {pred}\n')
+url = 'http://0.0.0.0:9696/predict'
+response = requests.post(url, json=inference_input).json()
+print(response)
 
-#response = requests.post(url, json=inference_input).json()
 
 #print(response)
