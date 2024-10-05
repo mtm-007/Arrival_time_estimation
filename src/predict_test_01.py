03688b494363127ee87b51636616b3ae911d923e
Merhawi <merhawitsegay007@gmail.com>
2024-10-05 01:09:31 +0000

    add

--- src/predict_test_01.py
+++ src/predict_test_01.py
@@ -3,13 +3,9 @@ import requests
 inference_input = {
     "PULocationID": 210,
     "DOLocationID": 69,
-    #"PUDO": '110_50',
     "trip_distance": 9.98
 }
 
 url = 'http://0.0.0.0:9696/predict'
 response = requests.post(url, json=inference_input).json()
 print(response)
-
-
-#print(response)
